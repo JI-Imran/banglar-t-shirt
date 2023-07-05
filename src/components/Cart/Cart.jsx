940af73553a16990cd711d412f114a0aafece099
JI-Imran <jiimran525@gmail.com>
2023-07-05 22:05:18 +0600

    added some conditional

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({ cart, handleRemoveFromCart }) => {
+    let message;
+    if (cart.length === 0) {
+        message = <p>Please add some products</p>
+    } else {
+        message =
+            <div>
+                <p><small>Thanks for giving your money</small></p>
+            </div>
+    }
     return (
         <div>
-            <h2>Order Summary: {cart.length}</h2>
+            <h2 className={cart.length === 1 ? 'blue': 'red'}>Order Summary: {cart.length}</h2>
+            <p className={`bold bolder ${cart.length === 3 ? 'yellow' : 'purple'}`}>Something</p>
+            {
+                cart.length > 2
+                    ? <small className='purple'>buy more..</small>
+                    : <small>poor</small>
+            }
+            {
+                message
+            }
             {
                 cart.map(tshirt => <p
                     key={tshirt._id}
@@ -13,8 +32,19 @@ const Cart = ({ cart, handleRemoveFromCart }) => {
                     >X</button>
                 </p>)
             }
+            {
+                cart.length === 2 && <p>Double bonaz</p>
+            }
+            {
+                cart.length === 3 
+            }
         </div>
     );
 };
 
 export default Cart;
+
+/**
+ * conditional rendering
+ * 1. use if else to set a variable that will contain an element, component
+ */
